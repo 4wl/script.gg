a330fb1991cb2ca426b69fff4306c6e41b635f8c
MrSerial <87231854+MrSerial@users.noreply.github.com>
2022-01-23 01:40:11 -0500

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,30 @@
+nouni=false
+local function load(l)
+loadstring(game:HttpGet(l,true))()
+nouni=true
+end
+if game.PlaceId==3102144307 then
+load("https://raw.githubusercontent.com/MrSerial/script.gg/main/SupportedGames/AnimeClickerSimulator.lua")
+end
+if game.PlaceId==5346447287 then
+load("")
+end
+if game.PlaceId==1962086868 then
+load("")
+end
+if game.PlaceId==155615604 then
+load("")
+end
+if game.PlaceId==1537690962 then
+load("")
+end
+if game.PlaceId==3101667897 then
+load("")
+end
+if game.PlaceId==142823291 then
+load("")
+end
+if nouni==false then
+load("")
+end
+-- // skidded from a skid that skidded a skid that skided
