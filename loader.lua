6c7c50a83b12799627ff8f610b8beb7712162b35
Kederal <87231854+Kederal@users.noreply.github.com>
2022-01-29 07:27:34 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -9,6 +9,9 @@ end
 if game.PlaceId==8554378337 then
 load("https://raw.githubusercontent.com/Kederal/script.gg/main/SupportedGames/Weapon%20Fighting%20Simulator.lua")
 end
+if game.PlaceId==7508789810 then
+load("https://raw.githubusercontent.com/Kederal/script.gg/main/SupportedGames/Devious%20Lick%20Simulator.lua")
+end
 if nouni==false then
 load("")
 end
