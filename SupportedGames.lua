bbd80230dc3b99998f5e9b1e903600804564a914
Kederal <87231854+Kederal@users.noreply.github.com>
2022-01-30 20:27:42 -0500

    Update SupportedGames.lua

--- SupportedGames.lua
+++ SupportedGames.lua
@@ -1,15 +1,19 @@
-[+] Weapon Fighting Simulator -- // 8554378337
+[+] Anime Clicker Simulator -- // 3102144307 | https://www.roblox.com/games/3102144307
+  [=] Autofarm
+    [-] AutoTap
+    [-] Auto Boss
+    [-] Auto Rebirth
+    [-] Auto Hatch
+    [-] Auto Daily Rewards
+  [=] Auto Upgrade
+    [-] Auto Achievements
+    [-] Auto Upgrade
+  [=] Misc
+    [-] Teleport To World
+
+[+] Weapon Fighting Simulator -- // 8554378337 | https://www.roblox.com/games/8554378337
   [=] Autofarm
     [-] Autofarm
       [-] Auto Collect
   [=] Misc
     [-] Free Gamepasses
-
-[+] Devious Lick Simulator -- // 7508789810
-  [=] Autofarm
-    [-] Autofarm
-    [-] Auto Sell
-    [-] Auto Hatch
-    [-] Auto Triple Hatch
-  [=] Misc
-    [-] Walkspeed & JumpPower
